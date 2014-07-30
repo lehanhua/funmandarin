33046a88aac06c1126ba3ab226b83761a4f33452
Lehan Huang <inf3ct10nz3r0@gmail.com>
2014-07-29 23:14:52 -0700

    updated codeigniter core

--- application/views/view_about.php
+++ application/views/view_about.php
@@ -2,6 +2,28 @@
 <div class="container container-top">
     <div class="page-header">
 		<h1>FunMandarin</h1>
+		<p>Fun Mandarin was developed to serve people who are interesting in learning Mandarin Chinese, 
+			and to provide guidance and structure for those interested in developing consciousness and understanding of Chinese culture.
+		</p>
+		<!--
+		<div class="banner" id="banner-about">
+		</div>
+		-->
     </div>
-    
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-1"></div>
+		<div class="col-md-8">
+			<p>Our purpose is to give students a fun and enjoyable language learning experience.  All of our teachers are native Mandarin speakers.  They have been teaching American children and adults for nearly ten years.
+			They have adopted western teaching methods and second language learning strategies such as TPR (Total Physical Response), along with traditional Confucian teaching spirits (respect, patience, consistency) to inspire and instruct our students.  
+			Therefore, we have created and developed a curriculum that is flexible and adaptable, and carefully fulfills each of our student?s needs.
+			</p>
+			<p>Although we know there are many Chinese schools and programs in the Bay Area, with many different teaching methods and materials in use by other Chinese teachers, students with limited or no Mandarin speaking background often have difficulty enjoying the process of learning Mandarin.  We have designed our stimulating and fun curricula especially for second language learners at various levels of experience.
+			</p>
+			<p>Our intercultural teaching methods utilize unique creative ideas and activities to help our students study Mandarin in a fun and efficient way.  We make our lessons enjoyable and educational, so students will excel in our language learning program.
+			</p>
+			<strong>Having fun with Mandarin is the goal for Fun Mandarin and for you!</strong>
+		</div>
+	</div>
 </div>
