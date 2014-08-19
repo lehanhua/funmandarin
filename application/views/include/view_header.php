de5b7e3492b0327c746bc56b36fbbcacda9843f1
Lehan Huang <lehan.huang@whitehatsec.com>
2014-08-19 15:30:52 -0700

    updated cdn links

--- application/views/include/view_header.php
+++ application/views/include/view_header.php
@@ -9,9 +9,9 @@
 
 	<title><?php echo $title ?></title>
 
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 	<link href="<?php echo base_url('assets/css/custom.css') ?>" rel="stylesheet">
-	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 	
 	<!-- add IE8 support stuff -->
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
