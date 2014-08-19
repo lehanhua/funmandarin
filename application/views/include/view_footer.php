b9bd3780c419096f657640a4e299844a4fb347cb
Lehan Huang <lehan.huang@whitehatsec.com>
2014-08-19 15:32:29 -0700

    fixed typo

--- application/views/include/view_footer.php
+++ application/views/include/view_footer.php
@@ -8,7 +8,7 @@
 <!-- Bootstrap core JavaScript ===================================== -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url('assets/js/custom.js') ?>"></script>
 </body>
 </html>
