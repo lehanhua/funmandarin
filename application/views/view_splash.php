ddfc4939e29090a56b56e31e980d8411bd45b544
Lehan Huang <lehan.huang@whitehatsec.com>
2014-08-22 17:16:59 -0700

    fixed typography issues on splash page

--- application/views/view_splash.php
+++ application/views/view_splash.php
@@ -1,13 +1,49 @@
 <!-- body -->
-<div class="jumbotron">
-	<div class="container container-top">
-		<h1>FunMandarin <small>趣味中文網</small></h1>
-		<p>Learning Mandarin can be EASY and FUN! <br />
-		   Enjoy our wonderful curriculum, excellent teachers, and flexible schedules!
-		</p>
-	</div>
-</div>
-<div class="container">
+<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="<?php echo base_url('assets/img/jumbotron-bg.jpg')?>" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>FunMandarin <small>趣味中文網</small></h1>
+              <p>Learning Mandarin can be EASY and FUN! <br />
+		   		Enjoy our wonderful curriculum, excellent teachers, and flexible schedules!
+			  </p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="<?php echo base_url('assets/img/jumbotron-bg.jpg')?>" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">See Daycare</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="<?php echo base_url('assets/img/jumbotron-bg.jpg')?>" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Open Classes</a></p>
+            </div>
+          </div>
+        </div>
+    </div>
+      <!-- Controls -->
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div><!-- /.carousel -->
+<div class="container" style="margin-top: 50px;">
 	<div class="row">
 		<div class="col-md-1">
 		</div>
