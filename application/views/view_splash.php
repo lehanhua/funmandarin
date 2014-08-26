0adcba089b7c3f4931d60eb41cecf764423e94de
Lehan Huang <inf3ct10nz3r0@gmail.com>
2014-08-25 20:42:56 -0700

    updated home page layout

--- application/views/view_splash.php
+++ application/views/view_splash.php
@@ -1,3 +1,4 @@
+</div><!-- end .container for full-width carousel -->
 <!-- body -->
 <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
       <!-- Indicators -->
@@ -11,7 +12,7 @@
           <img src="<?php echo base_url('assets/img/jumbotron-bg.jpg')?>" alt="First slide">
           <div class="container">
             <div class="carousel-caption">
-              <h1>FunMandarin <small>趣味中文網</small></h1>
+              <h1>FunMandarin </h1>
               <p>Learning Mandarin can be EASY and FUN! <br />
 		   		Enjoy our wonderful curriculum, excellent teachers, and flexible schedules!
 			  </p>
@@ -44,45 +45,31 @@
       <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
       <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
 </div><!-- /.carousel -->
-<div class="container" style="margin-top: 50px;">
-	<!-- three circle headings below carousel 
-	<div class="row">
-		<div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-          <h2>Heading</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-	</div> -->
+<div class="container" style="padding-top: 20px;">
 	<div class="row">
 		<div class="col-md-1">
 		</div>
 		<div class="col-md-8">
-			<!-- TODO: make look nicer -->
-			<p>Through songs, games, body movements, stories, arts & crafts, and hands-on activities, 
-			you will find Mandarin is such an easy language to learn. 
-			You will be immersed in a relaxed atmosphere with our native experienced teachers in a small group. 
-			You can explore the amazing history of China, its ancient literature, culture and traditions. </p>
-			<p>Many holidays and special events will be celebrated with food, music, and dance We will visit with special invited guests who will teach us Chinese Kung Fu, calligraphy, and Chinise painting. 
-			Field trips with unique themes are another part of our exciting program.We provide individual lessons and group classes for children and adults. 
-			You can also check customized lessons to let us know your request. Tell us what you need and we will design the class for you!</p>
-			<p>We know you will enjoy learning Chinese language and culture with FunMandarin.</p>
+			<h1>趣味中文網 FunMandarin</h1>
+			<p><strong>Welcome to FunMandarin - the best place to learn Mandarin in the Bay Area</strong></p>
+			<br />
+			<div class="row">
+			<div class="col-xs-3">
+				some picture here
+			</div>
+			<div class="col-xs-9">
+				<p>Through songs, games, body movements, stories, arts & crafts, and hands-on activities, 
+				you will find Mandarin is such an easy language to learn. 
+				You will be immersed in a relaxed atmosphere with our native experienced teachers in a small group. 
+				You can explore the amazing history of China, its ancient literature, culture and traditions. </p>
+				<p>Many holidays and special events will be celebrated with food, music, and dance We will visit with special invited guests who will teach us Chinese Kung Fu, calligraphy, and Chinise painting. 
+				Field trips with unique themes are another part of our exciting program.We provide individual lessons and group classes for children and adults. 
+				You can also check customized lessons to let us know your request. Tell us what you need and we will design the class for you!</p>
+				<p>We know you will enjoy learning Chinese language and culture with FunMandarin.</p>
+			</div>
+			</div><!-- end .row -->
 		</div>
-		<div class="col-md-1">
-		</div>
-		<div class="col-md-2">
+		<div class="col-md-3">
 			<h3>Latest News</h3>
 		</div>
 	</div>
