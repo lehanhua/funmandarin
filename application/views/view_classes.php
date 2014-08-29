ca31ba4288bea66b2bfe436c19c99f3e1d11d561
Lehan Huang <lehan.huang@whitehatsec.com>
2014-08-29 16:43:45 -0700

    added content to view_classes

--- application/views/view_classes.php
+++ application/views/view_classes.php
@@ -4,3 +4,23 @@
 			<h1>Classes</h1>
 		</div>
 	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-3 col-md-2 sidebar">
+				<ul class="nav nav-sidebar">
+					<li><a href="#">Chinese Culture Class</a></li>
+					<li><a href="#">Children's Beginning Mandarin Class</a></li>
+					<li><a href="#">Chinese Lessons</a></li>
+					<li><a href="#">Private Lessons</a></li>
+					<li><a href="#">Simple Chinese Cooking</a></li>
+					<li><a href="#">Mandarin Enrichment Program</a></li>
+					<li><a href="#">Fun with Hanyu Pinyin</a></li>
+				</ul>
+			</div><!-- end .sidebar -->
+			<div class="col-sm-9 col-md-10 main">
+				<h3>Chinese Culture Class</h3>
+				<p class="text-justify">Are you interested in Chinese culture, Chinese art and music, history, traditions, and food?  This is an introductory class for 
+					children and adults who would like to explore this ancient country from the past through the present time in a variety of ways.</p>
+			</div>
+		</div>
+	</div>
