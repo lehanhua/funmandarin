d164c06a825aafec5387e88fe69caf2010e341f3
Lehan Huang <lehan.huang@whitehatsec.com>
2015-08-10 16:45:25 -0700

    update jquery

--- application/views/include/view_footer.php
+++ application/views/include/view_footer.php
@@ -12,8 +12,8 @@
 </footer>
 
 <!-- Bootstrap core JavaScript ===================================== -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://code.jquery.com/ui/1.10.4/jquery-ui.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url('assets/js/custom.js') ?>"></script>
 </body>
