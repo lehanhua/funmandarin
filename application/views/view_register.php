84121b6e61b1c9bba12cca1601a660cc05f5c86c
Lehan Huang <lehan.huang@whitehatsec.com>
2015-08-10 17:07:49 -0700

    update register page template with class selection

--- application/views/view_register.php
+++ application/views/view_register.php
@@ -8,12 +8,22 @@
 	<div class="container">
 		<!-- form selection here -->
 		<div class="row">
+		<div class="col-md-12">
 			<form class="form-inline">
+				<div class="form-group">
+					<label for="selectClass">Select a class</label>
+      				<select id="selectClass" class="form-control">
+        				<option>Fun with Hanyu Pinyin</option>
+        				<option>Class two</option>
+        				<option>Class three</option>
+      				</select>
+				</div>
 			</form>
+		</div>
 		</div><!-- end .row -->
-		<?= validation_errors() ?>
 		<div class="row">
 			<div class="col-md-6">
+				<?= validation_errors() ?>
 				<?= form_open('register') ?>
 				<div class="form-group">
 					<label for="firstname">First Name</label>
@@ -24,5 +34,9 @@
 				<input type="submit" class="btn btn-default" name="submit" value="Register" />
 				</form>
 			</div><!-- end .col-md-6 -->
+			<div class="col-md-6">
+				<h2>Class date (Winter 2015)</h2>
+				<p>test text</p>
+			</div>
 		</div><!-- end .row -->
 	</div><!-- end .container -->
