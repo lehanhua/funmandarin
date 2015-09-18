b0ac1155874797a113ad8dbab776c12b18cf1178
Lehan Huang <lehan.huang@whitehatsec.com>
2015-09-18 15:31:36 -0700

    add bootstrap select resources

--- application/views/include/view_header.php
+++ application/views/include/view_header.php
@@ -10,8 +10,9 @@
 
 	<title><?php echo $title ?></title>
 
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.3/css/bootstrap-select.min.css">
 	<link href="<?php echo base_url('assets/css/custom.css') ?>" rel="stylesheet">
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 	
