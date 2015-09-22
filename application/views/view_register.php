1141f5c8bcd5fa96c83b3cbb298448438ecdb67a
Lehan Huang <lehan.huang@whitehatsec.com>
2015-09-21 18:38:33 -0700

    add show and hide effects to school select

--- application/views/view_register.php
+++ application/views/view_register.php
@@ -13,23 +13,30 @@
 		</div>
 	</div>
 	<div class="container">
-		<div class="row">
-			<div class="col-md-8 main">
-				<div id="cumberland" style="display:none">
-					<h2>Cumberland School</h2>
-					<p>
-  						<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>	
-						Cumberland Elementary School, 824 Cumberland Drive, Sunnyvale, CA 94087 <a href="#" aria-hidden="true">(show map)</a><p>
-						<div class="col-md-4">
-							map
-						</div>
-				</div><!-- end #cumberland -->
-				<div id="Hilldale" style="display:none">
-					<h2>Hilldale School</h2>
-					<p>
-  						<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>	
-						Hilldale School, 79 Florence Street, Daly City, CA 94014 <a href="#" aria-hidden="true">(show map)</a><p>
-				</div><!-- end #hilldale -->
-			</div><!-- end .main -->
-		</div><!-- end .row -->
+		<div class="school-info Cumberland" style="display:none">
+			<div class="row">
+				<div class="col-md-8 main">
+						<h2>Cumberland School</h2>
+						<p>
+	  						<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>	
+							Cumberland Elementary School, 824 Cumberland Drive, Sunnyvale, CA 94087 <a href="#" aria-hidden="true">(show map)</a><p>
+				</div>
+				<div class="col-md-4">
+					map
+				</div>
+			</div><!-- end .row -->
+		</div><!-- end .school-info .Cumberland -->
+		<div class="school-info Hilldale" style="display:none">
+			<div class="row">
+				<div class="col-md-8 main">
+						<h2>Hilldale School</h2>
+						<p>
+	  						<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>	
+							Hilldale School, 79 Florence Street, Daly City, CA 94014 <a href="#" aria-hidden="true">(show map)</a><p>
+					</div>
+				<div class="col-md-4">
+					map
+				</div>
+			</div><!-- end .row -->
+		</div><!-- end .school-info .Hilldale -->
 	</div><!-- end .container -->
