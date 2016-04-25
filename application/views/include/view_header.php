8b470662cca111747e7349b5d3880a88237f1d66
Lehan <lehan.huang@salesforce.com>
2016-04-24 20:29:12 -0700

    various changes

--- application/views/include/view_header.php
+++ application/views/include/view_header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="keywords" content="">
@@ -9,8 +10,9 @@
 
 	<title><?php echo $title ?></title>
 
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.3/css/bootstrap-select.min.css">
 	<link href="<?php echo base_url('assets/css/custom.css') ?>" rel="stylesheet">
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 	
--- application/views/view_register.php
+++ application/views/view_register.php
@@ -2,41 +2,120 @@
 	<div class="container container-top">
 		<div class="page-header">
 			<h1>Register</h1>
-			<p>Fill out the form below to register for <?= date("Y") ?> classes</p>
+			<p>Select a school to register for <?= date("Y") ?> classes</p>
+			<select class="selectpicker" data-style="btn-primary">
+				<option>Cumberland</option>
+  				<option>Hilldale</option>
+  				<option>Synapse</option>
+  				<option>AppleSeed</option>
+			</select><br />
 		</div>
 	</div>
 	<div class="container">
-		<!-- form selection here -->
-		<div class="row">
-		<div class="col-md-12">
-			<form class="form-inline">
-				<div class="form-group">
-					<label for="selectClass">Select a class</label>
-      				<select id="selectClass" class="form-control">
-        				<option>Fun with Hanyu Pinyin</option>
-        				<option>Class two</option>
-        				<option>Class three</option>
-      				</select>
+		<div class="school Cumberland">
+			<div class="row school-info">
+				<div class="col-md-8 main">
+					<h2 style="margin-bottom:20px;margin-top:10px">Cumberland School</h2>
+					<p>
+  						<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>	
+						Cumberland Elementary School, 824 Cumberland Drive, Sunnyvale, CA 94087 <a href="#" aria-hidden="true">(show map)</a>
+					</p>
+					<div class="school-description section-indent">
+						<p>✤ Fun Mandarin was developed to serve people who are interested in learning Mandarin Chinese, and to provide guidance and structure for those interested in developing consciousness and understanding of Chinese culture.</p>
+						<p>✤ Fun Mandarin has created motivational techniques for children to learn Mandarin while having fun with interactive activities such games, songs, body movements, and stories etc. We emphasize repetition to achieve correct pronunciation. Students will have the chance to interact with their peers to practice communicating in their new language.</p>
+						<p>✤ Our intercultural teaching methods utilize unique creative ideas and activities to help our students study Mandarin in a fun and efficient way. We make our lesson enjoyable and educational, so students will excel in our language learning program. 
+						</p>
+					</div>
 				</div>
-			</form>
-		</div>
-		</div><!-- end .row -->
-		<div class="row">
-			<div class="col-md-6">
-				<?= validation_errors() ?>
-				<?= form_open('register') ?>
-				<div class="form-group">
-					<label for="firstname">First Name</label>
-					<input type="input" name="firstname" class="form-control" value="<?= set_value('firstname')?>" placeholder="First Name"/><br />
-					<label for="lastname">Last Name</label>
-					<input type="input" name="lastname" class="form-control" value="<?= set_value('lastname')?>" placeholder="Last Name"/><br />
+				<div class="col-md-4">
+					map
 				</div>
-				<input type="submit" class="btn btn-default" name="submit" value="Register" />
-				</form>
-			</div><!-- end .col-md-6 -->
-			<div class="col-md-6">
-				<h2>Class date (Winter 2015)</h2>
-				<p>test text</p>
+			</div><!-- end .row .school-info -->
+			<br />
+			<div class="row">
+				<div class="col-md-10">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h2 class="panel-title">Conversational <small>10/01/2015 - 12/17/2015</small></h2>
+						</div>
+						<div class="panel-body">
+							<p class="text-info">Conversational Mandarin: 3rd-5th graders (daily life applications, theme-based, equivalent to adult class of Mandarin for travelers; 3rd graders must have taken Level 3 or equivalent, 4th and 5th graders with no Mandarin knowledge may enroll)
+							</p>
+							<p><!--<span class="glyphicon glyphicon-time" style="font-size: 20px" aria-hidden="true"></span>--> Registration Period: 9/14/2015 - 10/14/2015 </p>
+							<p class="section-indent">Tuition: $170.00 </p>
+							<a href="<?= site_url('register/classes/conversational') ?>" class="btn btn-success pull-right" target="_blank">Register</a>
+						</div>
+					</div>
+					<br />
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Mandarin Level 1 <small>10/02/2015 - 12/18/2015</small></h3>
+						</div>
+						<div class="panel-body">
+							<p class="text-info">FunMandarin Level 1: K-2nd graders (no Mandarin knowledge required). Class size is 8 to 14 children.</p>
+							<p>Registration Period: 9/14/2015 - 10/14/2015 </p>
+							<p class="section-indent">Tuition: $170.00 </p>
+							<a href="#" class="btn btn-success pull-right" target="_blank">Register</a>
+						</div>
+					</div>
+					<br />
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Mandarin Level 2 <small>10/02/2015 - 12/18/2015</small></h3>
+						</div>
+						<div class="panel-body">
+							<p class="text-info">FM Level 2: K-3rd graders (children who took Level 1 last year or equivalent: some reading and writing, with previous Mandarin class experience; 3rd graders with no Mandarin knowledge may enroll)</p>
+							<p>Registration Period: 9/14/2015 - 10/14/2015 </p>
+							<p class="section-indent">Tuition: $170.00 </p>
+							<a href="#" class="btn btn-success pull-right" target="_blank">Register</a>
+						</div>
+					</div>
+					<br />
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Mandarin Level 3 <small>10/02/2015 - 12/18/2015</small></h3>
+						</div>
+						<div class="panel-body">
+							<p class="text-info">K-3rd graders (children who took Level 1 last year or equivalent: some reading and writing, with previous Mandarin class experience 3rd graders with no Mandarin knowledge may enroll)</p>
+							<p>Registration Period: 9/14/2015 - 10/14/2015 </p>
+							<p class="section-indent">Tuition: $170.00 </p>
+							<a href="#" class="btn btn-success pull-right" target="_blank">Register</a>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div><!-- end .row -->
+		</div><!-- end .school .Cumberland -->
+		<div class="school Hilldale" style="display:none">
+			<div class="row school-info">
+				<div class="col-md-8 main">
+						<h2>Hilldale School</h2>
+						<p>
+	  						<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>	
+							Hilldale School, 79 Florence Street, Daly City, CA 94014 <a href="#" aria-hidden="true">(show map)</a><p>
+					</div>
+				<div class="col-md-4">
+					map
+				</div>
+			</div><!-- end .school .school-info -->
+			<br /><br />
+			<div class="row">
+				<div class="col-md-10">
+					<h3>Hilldale Level 1 <small>8/25/2015 - 10/22/2015</small></h3>
+					<p class="section-indent">No prior Mandarin experience required. Students will be introduced to the Mandarin language and Chinese culture. Our main focus is to develop students’ listening and speaking skills. Students will learn words and short sentences while practicing the four tones of the Chinese language. Teachers will use a variety of fun and engaging material and will encourage students to speak while playing games, learning songs, and working with Chinese crafts and more.</p>
+					<p class="section-indent">Registration Period: 8/24/2015 - 9/27/2015 </p>
+					<p class="section-indent">Tuition: $375.00 </p>
+					<a href="#" class="btn btn-success pull-right" target="_blank">Register</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-10">
+					<h3>Hilldale Level 1 <small>8/25/2015 - 10/22/2015</small></h3>
+					<p class="section-indent">This is a continuation of Intermediate Chinese from last quarter. Students should be able to carry on a simple conversation using the words and sentences acquired from their previous experiences. Students will further develop their listening and speaking skills through fun and engaging games and interactive activities. Based on their age group, students will learn to read and write simple Chinese characters.</p>
+					<p class="section-indent">We encourage to use the online learning tool to enhance their Mandarin skills at home. Further instructions will be sent to the parents when the registration in completed.</p>
+					<p class="section-indent">Registration Period: 8/23/2015 - 9/27/2015 </p>
+					<p class="section-indent">Tuition: $375.00 </p>
+					<a href="#" class="btn btn-success pull-right" target="_blank">Register</a>
+				</div>
+			</div>
+		</div><!-- end .school .Hilldale -->
 	</div><!-- end .container -->
